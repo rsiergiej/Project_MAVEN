d11ec7aa42ac7cdfdc7c725d5c75cfd7b9732fbb
Radosław <r.siergiej@omtech.pl>
2019-03-18 10:28:28 +0100

    wip

--- src/main/java/application/controllers/LoginController.java
+++ src/main/java/application/controllers/LoginController.java
@@ -6,10 +6,16 @@ import application.model.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 @Controller
 public class LoginController {
@@ -17,6 +23,8 @@ public class LoginController {
 
     @Autowired
     private UserRepository userRepository;
+
+
 
     @RequestMapping(value = "/register.html", method = RequestMethod.GET)
     public String register(Model model)
@@ -28,17 +36,40 @@ public class LoginController {
         userDto.setEmail("ww@gmail.com");
         model.addAttribute("user", userDto);
 
-        System.out.println(userDto.getFirstName());
+       // System.out.println(userDto.getFirstName());
         return "register.html";
     }
 
 
     @PostMapping("/register.html")
-    public String registerPOST(@ModelAttribute(value="user") UserDTO userDTO)
+    public String registerPOST(@ModelAttribute(value="user") UserDTO userDTO, BindingResult bindingResult)
     {
+//  https://hellokoding.com/registration-and-login-example-with-spring-xml-configuration-maven-jsp-and-mysql/
+// http://websystique.com/spring-security/spring-security-4-password-encoder-bcrypt-example-with-hibernate/
+
+
+//        try {
+//            MessageDigest md = MessageDigest.getInstance("SHA-256");
+//            byte[] hashInBytes = md.digest(userDTO.getPassword().getBytes(StandardCharsets.UTF_8));
+//
+//            StringBuilder sb = new StringBuilder();
+//            for (byte b : hashInBytes) {
+//                sb.append(String.format("%02x", b));
+//            }
+//            System.out.println("SZYFROWANIE: " + sb.toString());
+//
+//            String encoded = Base64.getEncoder().encodeToString(hashInBytes);
+//                    System.out.println("ENCODE: " + encoded);
+//
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+
+
+
 
         userRepository.save(userDTO);
-        System.out.println(userDTO.getLogin());
+        System.out.println(userDTO.toString());
         return "register.html";
     }
 }
--- src/main/java/application/model/UserDTO.java
+++ src/main/java/application/model/UserDTO.java
@@ -1,12 +1,10 @@
 package application.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -17,8 +15,10 @@ import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Valid
 public class UserDTO {
 
     @Id
@@ -48,4 +48,25 @@ public class UserDTO {
         this.email =email;
 
     }
+
+//
+//    public void validate(Errors errors) {
+//
+//
+//        if (this.login.length() < 4 || this.login.length() > 32) {
+//            errors.rejectValue("login", "Size.userForm.login");
+//        }
+//        if (this.findByUsername(user.getUsername()) != null) {
+//            errors.rejectValue("username", "Duplicate.userForm.username");
+//        }
+//
+//        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+//        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+//            errors.rejectValue("password", "Size.userForm.password");
+//        }
+//
+//        if (!user.getPasswordConfirm().equals(user.getPassword())) {
+//            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+//        }
+//    }
 }
