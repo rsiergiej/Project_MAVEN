e666c1b86d0997a84d8c2b001ffc25ffea8c888c
Radosław <r.siergiej@omtech.pl>
2019-03-21 11:33:41 +0100

    aes 128 ecb final version

--- src/main/java/application/security/AES.java
+++ src/main/java/application/security/AES.java
@@ -1,52 +1,53 @@
 package application.security;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+
 
 public class AES {
-
-
+    // AES 128  ECB
     private static final byte[] key = "testaesKEY0value".getBytes(StandardCharsets.UTF_8);
     private static final String ALGORITHM = "AES";
+
+
+    public static void main (String[] args) throws Exception
+    {
+        String str = "123456789asdtestTE!@#$%^&*()+{}:?<~` `";
+        AES aesTest = new AES();
+
+        String enc = aesTest.encrypt(str);
+        System.out.println(enc);
+        String decrypt = aesTest.decrypt(enc);
+        System.out.println(decrypt);
+    }
 
     public AES()
     {
 
     }
 
-    public byte[] encrypt(String plainText) throws Exception
+    public String encrypt(String encryptStr) throws Exception
     {
-        byte[] textToEncrypt = plainText.getBytes();
-
+        byte[] textToEncrypt = encryptStr.getBytes();
         SecretKeySpec secretKey = new SecretKeySpec(key, ALGORITHM);
+
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         textToEncrypt = cipher.doFinal(textToEncrypt);
-
-        return  textToEncrypt;
+        return  Base64.encodeBase64String(textToEncrypt);
 
     }
 
-
-//    public byte[] encrypt(byte[] plainText) throws Exception
-//    {
-//
-//
-//        SecretKeySpec secretKey = new SecretKeySpec(key, ALGORITHM);
-//        Cipher cipher = Cipher.getInstance(ALGORITHM);
-//        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-//        return cipher.doFinal(plainText);
-//    }
-
-
-    public byte[] decrypt(byte[] cipherText) throws Exception
+    public String decrypt(String decryptStr) throws Exception
     {
         SecretKeySpec secretKey = new SecretKeySpec(key, ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
-        return cipher.doFinal(cipherText);
+        byte[] decrypt = cipher.doFinal(Base64.decodeBase64(decryptStr));
+        return new String(decrypt);
     }
+
 }
---
+++ src/main/java/application/security/BCrypt.java
@@ -0,0 +1,6 @@
+package application.security;
+
+public class BCrypt {
+
+
+}
